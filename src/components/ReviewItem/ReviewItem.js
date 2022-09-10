c7382422a9fe750f6490dac23a612f298e38a768
MD Siful Islam Bappy <bappy.ac.bd@gmail.com>
2022-09-10 18:34:15 +0900

    review item section is complete

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,3 +1,5 @@
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./ReviewItem.css";
 
@@ -14,17 +16,22 @@ const ReviewItem = (props) => {
             {name.length > 20 ? name.slice(0, 20) + "..." : name}
           </p>
           <p>
-            Price: <span className="orange-color">{price}</span>
+            Price: <span className="orange-color">${price}</span>
           </p>
           <p>
-            <small>Shipping: {shipping}</small>
+            <small>Shipping: ${shipping}</small>
           </p>
           <p>
             <small>Quantity: {quantity}</small>
           </p>
         </div>
         <div className="delete-container">
-          <button>Delete</button>
+          <button className="delete-button">
+            <FontAwesomeIcon
+              className="delete-icon"
+              icon={faTrashAlt}
+            ></FontAwesomeIcon>
+          </button>
         </div>
       </div>
     </div>
