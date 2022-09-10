001e3fe58cc5c62127d13ab6e273633c110d0f4f
MD Siful Islam Bappy <bappy.ac.bd@gmail.com>
2022-09-10 15:31:45 +0900

    route is been set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Invertory/Inventory';
+import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
 function App() {
@@ -9,9 +11,10 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Shop></Shop>}></Route>
-        <Route path='/home' element={<Shop></Shop>}></Route>
+        <Route path='/shop' element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
       </Routes>
-      <Shop></Shop>
     </div>
   );
 }
