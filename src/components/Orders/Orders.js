23fb08765f1e4dee79704993750181a8ad28e23b
MD Siful Islam Bappy <bappy.ac.bd@gmail.com>
2022-09-10 18:09:29 +0900

    order page working on

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    console.log(cart);
     return (
-        <div>
-            <h2>This is orders: {products.length}</h2>
+        <div className="shop-container">
+            <div className="review-items-container">
+                {
+                    cart.map(product => <ReviewItem
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
